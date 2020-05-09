6c2f7dd9ee16b49a2b262d48fbbe4dcf3c2c2733
Carlos Tojal <carlos.tojal@hotmail.com>
2020-05-09 14:45:01 +0100

    basic autofill

--- app/src/main/java/com/fca/keysafe/MainActivity.java
+++ app/src/main/java/com/fca/keysafe/MainActivity.java
@@ -1,10 +1,16 @@
 package com.fca.keysafe;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.view.autofill.AutofillManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         list.setAdapter(accountAdapter);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,14 +39,16 @@ public class MainActivity extends AppCompatActivity {
 
         list = findViewById(R.id.list);
 
+        Intent intent;
+
         // the user was not created or the pin is default
         if(new Helpers().getUser(this).getPin().equals("0000")) {
-            Intent intent = new Intent(this, CreatePin.class);
-            startActivity(intent);
+            intent = new Intent(this, CreatePin.class);
         } else { // login
-            Intent intent = new Intent(this, Login.class);
-            startActivity(intent);
+            intent = new Intent(this, Login.class);
         }
+
+        startActivity(intent);
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
